22d7a66aaa02395a3257b30659c4c7d3e3543c74
mrtovsky <zakrzewski.mateusz95@gmail.com>
2018-12-02 02:15:02 +0100

    @modify: Extension of the TargetEncoder estimator for cross-validation while determining the mean target values as well as addition of regularization parameter.

--- encoding.py
+++ encoding.py
@@ -3,24 +3,44 @@ import pandas as pd
 
 from inspect import currentframe, getargvalues
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import KFold
 
 
 class TargetEncoder(BaseEstimator, TransformerMixin):
     """Encodes categorical features with the corresponding target aggregated
      value.
 
-    Accepts DataFrames with categorical features only. 
-    
+     If cv param is specified, performs determination of mean values on the way
+     of cross validation within inner cross validation. As a result for each of
+     the outside folds received target aggregated values will be less biased. 
+
     Parameters
     ----------
-    encoding_func: function or string {'mean', 'median'} (default: 'mean')
-        Function passed to the agg method called on the GroupBy object
-        on the basis of which the coding is made.
-    
+    columns: list
+        List of DataFrame columns' names on which target encoding should be
+        performed. If not specified all categorical columns are taken.
+
     nan_as_category: boolean (default: True)
         If True includes NaNs as one of the categories and also applies 
-        mean encoding for this subgroup.
-        
+        target encoding for this subgroup.
+    
+    cv: int
+        Number of cross-validation folds. 
+
+    inner_cv: int
+        Number of inner cross-validation folds.
+
+    shuffle: boolean (default: True)
+        Whether to shuffle the data before splitting into batches.
+
+    alpha: int (default: 5)
+        Regularization value (times of global mean added to the weighted mean
+        of each category). The larger, the more conservative the algorithm
+        will be. If you want to use the standard mean just set alpha to 0.
+    
+    random_state: int
+        Random state for sklearn algorithms.
+
     Attributes
     ----------
     cat_aggval_: dict, length = n_cat_features
@@ -30,7 +50,10 @@ class TargetEncoder(BaseEstimator, TransformerMixin):
         target aggregated value.
     
     """
-    def __init__(self, encoding_func='mean', nan_as_category=True):
+    def __init__(self, columns=None, nan_as_category=True, 
+                 cv=None, inner_cv=None, shuffle=True, 
+                 alpha=5, random_state=None):
+        
         icf = currentframe()    
         args, _, _, values = getargvalues(icf)
         values.pop('self')
@@ -58,29 +81,42 @@ class TargetEncoder(BaseEstimator, TransformerMixin):
         assert isinstance(X, pd.DataFrame), \
             'Input must be an instance of pandas.DataFrame()!'
         assert len(X) == len(y), 'X and y must be the same length!'
-        
+
+        if self.columns is None:
+            self.columns = X.select_dtypes('category').columns.tolist()
+
         self.cat_aggval_ = {}
         df = X.assign(target=y)
-        
-        for col in X.columns:
-            agg_dict = df.groupby(col).target.agg(self.encoding_func).to_dict()
-            if self.nan_as_category:
-                nan_val = df[df[col].isnull()].target.agg(self.encoding_func)
-                agg_dict[np.nan] = nan_val
+
+        for col in self.columns:
+            # TODO: define own agg function with additional alpha regularization
+            agg_dict = df.groupby(col).target.agg(self._penalized_mean,
+                                                  y=y,
+                                                  alpha=self.alpha).to_dict()
+            if self.nan_as_category and df[col].isnull().sum() > 0:
+                nan_val = df[df[col].isnull()].target.agg(self._penalized_mean,
+                                                          y=y,
+                                                          alpha=self.alpha)
+                agg_dict['NaNCategory'] = nan_val
             self.cat_aggval_[col] = agg_dict
         
         return self
     
-    def transform(self, X):
+    def transform(self, X, y=None):
         """Applies target encoding on X.
 
-        X is target encoded with the aggregated values kept in the cat_aggval_.
+        X is target encoded with the aggregated values kept in the cat_aggval_
+        and for the training data encoding is made with additional spread
+        obtained in the cross-validation within cross-validation.
 
         Parameters
         ----------
         X: DataFrame, shape (n_samples, n_features)
             New data with n_samples as its number of samples.
-
+        
+        y: array-like, shape (n_samples, )
+            Vector of target variable values corresponding to X data.
+        
         Returns
         -------
         X_new: DataFrame, shape (n_samples, n_features)
@@ -97,8 +133,97 @@ class TargetEncoder(BaseEstimator, TransformerMixin):
         assert isinstance(X, pd.DataFrame), \
             'Input must be an instance of pandas.DataFrame()!'
         
-        X_new = pd.DataFrame()
-        for col in X.columns:
-            X_new[col] = X[col].map(self.cat_aggval_[col])
-        
+        X_new = X.copy()
+
+        if self.nan_as_category:
+            missing_col = X_new.columns[X_new.isnull().any()].tolist()
+            col_to_fill = list(set(self.columns).intersection(missing_col))
+            for col in col_to_fill:
+                if X_new[col].dtype == 'category':
+                    X_new[col] = X_new[col].cat.add_categories('NaNCategory')
+            X_new[col_to_fill] = X_new[col_to_fill].fillna('NaNCategory')    
+
+        if y is None or self.cv is None:
+            for col in self.columns:
+                X_new[col] = X[col].map(self.cat_aggval_[col])
+
+        else: 
+            assert self.inner_cv is not None, \
+                ('When cv param is specified you must assign a value to the '
+                 'inner_cv param as well!')
+            X_new[self.columns] = self._transform_train_cv(X_new[self.columns], 
+                                                           y)
+            
         return X_new
+
+    def _transform_train_cv(self, X, y):
+        """This method is only applied for a training set. 
+
+        By using only the part of the data (k-1 folds) it estimates the encoding
+        value for the leftover fold. It performs those activities independently 
+        for each category. As a result we get synthetic values located more 
+        dense in a given space. 
+
+        """
+        X_new = pd.DataFrame(index=X.index, columns=X.columns)
+        df = X.assign(target=y)
+
+        kf = KFold(n_splits=self.cv, shuffle=self.shuffle,
+                   random_state=self.random_state)
+
+        for train_idx, encode_idx in kf.split(df):
+            inner_kf = KFold(n_splits=self.inner_cv, shuffle=self.shuffle,
+                             random_state=self.random_state)
+           
+            # Declaring list of DataFrames that will store inner mean values.
+            mean_df_list = [pd.DataFrame({f'{col}': X[col].unique()}) 
+                            for col in X.columns]
+            inner_df = df.iloc[train_idx].copy()
+            for loop_idx, (inner_train_idx, _) in \
+              enumerate(inner_kf.split(inner_df)):
+                for idx, col in enumerate(X.columns):
+                    agg_values = pd.DataFrame(inner_df.iloc[inner_train_idx]\
+                                 .groupby(col).target\
+                                 .agg('mean'))\
+                                 .reset_index()
+                    mean_df_list[idx] = mean_df_list[idx]\
+                                        .merge(
+                                               agg_values, 
+                                               how='left', 
+                                               on=f'{col}',  
+                                               suffixes=(f'_{loop_idx-1}', 
+                                                         f'_{loop_idx}')
+                                               )
+            mean_df_list = [data.set_index(data.columns[0])\
+                            .agg(self._penalized_mean,
+                                 axis=1, 
+                                 n_instances=X.iloc[train_idx, col_idx].count(), 
+                                 y=y[train_idx],
+                                 alpha=self.alpha * (1 - 1/self.cv)).to_dict()
+                            for col_idx, data in enumerate(mean_df_list)]
+            
+            for loop_idx, col in enumerate(X_new.columns):
+                X_new[col].iloc[encode_idx] = X[col].map(mean_df_list[loop_idx])
+
+        return X_new 
+
+    @staticmethod
+    def _penalized_mean(series, y, alpha, n_instances=None):
+        """Further regularization. 
+
+        Adds alpha value multiplied by the mean of the whole training 
+        data to the weighted average. 
+        
+        Formula: (p_c * n_c + p_global * alpha) / (n_c + alpha), where: 
+            p_c: mean for a category
+            n_c: number of instances in a category
+            p_global: global target mean
+            alpha: regularization parameter
+
+        """
+        if n_instances is None:
+            n_instances = series.count()
+        numerator = np.mean(series) * n_instances + np.mean(y) * alpha
+        denominator = n_instances + alpha 
+        return numerator / denominator
+        
