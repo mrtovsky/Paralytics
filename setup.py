228f28d62f1c2978d356caf4198a6f2d53156edb
mrtovsky <zakrzewski.mateusz95@gmail.com>
2018-12-02 03:40:11 +0100

    @create: Setup file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup 
+
+
+setup(
+    name='paralytics',
+    version='0.1',
+    author='Mateusz Zakrzewski',
+    description='Python Analytical Scripts.',
+    long_description=open('README.md').read(),
+    url='https://github.com/mrtovsky/Paralytics',
+    license='MIT',
+    packages=['paralytics'],
+    zip_safe=False
+)
