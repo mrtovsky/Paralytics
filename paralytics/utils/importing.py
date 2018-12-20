881ab095378aa43ef3003bda03091f19db394b91
mrtovsky <emerte.quant@gmail.com>
2018-12-20 23:42:41 +0100

    @create: Import utilities with a function: get_csv.

---
+++ paralytics/utils/importing.py
@@ -0,0 +1,17 @@
+import csv
+
+from collections import OrderedDict
+
+
+def get_csv(in_filename, cols_to_keep=None, dtype=float):
+    """Get csv as list of OrderedDicts."""
+    
+    with open(in_filename, encoding='UTF-8') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        if cols_to_keep is None:
+            cols_to_keep = reader.fieldnames
+        data = [
+            OrderedDict((key, dtype(row[key])) for key in cols_to_keep) 
+            for row in reader
+        ]
+    return data
