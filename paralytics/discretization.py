267f3eabb5a40d3bbca0b56aba033168bd693325
mrtovsky <emerte.quant@gmail.com>
2019-03-01 00:27:26 +0100

    @modify: Default method changed to 'sapling'. @fix: Correct grouping for sparse numeric cariables.

--- paralytics/discretization.py
+++ paralytics/discretization.py
@@ -13,10 +13,10 @@ from .utils.validation import is_numeric
 
 class Discretizer(BaseEstimator, TransformerMixin):
     """Discretizes variables in a given data set.
-    
-    Reduces continuous variables to a finite number of intervals with use of 
+
+    Reduces continuous variables to a finite number of intervals with use of
     declared methods.
-    
+
     Parameters
     ----------
     method: string {'sapling', 'spearman'}
@@ -25,16 +25,15 @@ class Discretizer(BaseEstimator, TransformerMixin):
         spearman:
             Submethod based on the Spearman's rang correlation. Divides the
             values into subsequent quartiles as long as it doesn't get full
-            monotonicity. If this doesn't happen, it divides values with use 
+            monotonicity. If this doesn't happen, it divides values with use
             of quantiles into the declared minimum number of buckets.
-            Using this method with parameter formula set to 'median' may throw 
+            Using this method with parameter formula set to 'median' may throw
             RuntimeWarning for fixed vector values in one of the input vectors
             becuase there is no point in tracking mutual change of two vectors
-            when one vector doesn't change. In this case you can run your code
-            with use of this command: python -W ignore foo.py 
+            when one vector doesn't change.
 
     formula: string {'mean', 'median'} (default: 'mean')
-        Chooses the formula that representatives will be chosen to check the 
+        Chooses the formula that representatives will be chosen to check the
         Spearman's rank correlation.
 
     max_bins: int (default: 20)
@@ -47,22 +46,22 @@ class Discretizer(BaseEstimator, TransformerMixin):
         Specifies maximum tree depth.
 
     min_samples_leaf: float (default: .05)
-        Specifies the minimum part of the entire population that must be 
+        Specifies the minimum part of the entire population that must be
         included in the leaf.
 
     Attributes
     ----------
     bins_: dictionary, length = n_features
-        Dictionary of upper limits of successive intervals excluding the 
+        Dictionary of upper limits of successive intervals excluding the
         maximum value which length equals the number of features in the data
         passed.
 
     """
 
-    def __init__(self, method='spearman', formula='mean', 
+    def __init__(self, method='sapling', formula='mean',
                  max_bins=20, min_bins=3, max_tree_depth=None,
                  min_samples_leaf=.05):
-        icf = inspect.currentframe()    
+        icf = inspect.currentframe()
         args, _, _, values = inspect.getargvalues(icf)
         values.pop('self')
 
@@ -71,7 +70,7 @@ class Discretizer(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None, **params):
         """Fit the binning with X by extracting upper limits of right-closed
-        intervals. 
+        intervals.
 
         Parameters
         ----------
@@ -85,7 +84,7 @@ class Discretizer(BaseEstimator, TransformerMixin):
         -------
         self: object
             Returns the instance itself.
-        
+
         """
         assert isinstance(X, pd.DataFrame), \
             'Input must be an instance of pandas.DataFrame()'
@@ -93,16 +92,16 @@ class Discretizer(BaseEstimator, TransformerMixin):
         call_method = getattr(self, self.method)
         self.bins_ = {}
         for col in X.columns.values:
-            # Checking whether columns are non-binary numeric (excluding nans) 
+            # Checking whether columns are non-binary numeric (excluding nans)
             if is_numeric(X[col]):
                 try:
                     self.bins_[col] = call_method(
                         X[col], np.asarray(y).ravel(), **params
-                    )
+                    ).astype(float)
                 except UniqueValuesError as e:
                     e.args += (f'The problem occured for the column: {col}.',)
-                    print(e)
-                    self.bins_[col] = np.unique(X[col].astype(str))
+                    print(' '.join(e.args))
+                    self.bins_[col] = np.unique(X[col]).astype(float)
             else:
                 self.bins_[col] = np.unique(X[col].astype(str))
 
@@ -112,18 +111,18 @@ class Discretizer(BaseEstimator, TransformerMixin):
         """Apply discretization on X.
 
         X is projected on the bins previously extracted from a training set.
-        
+
         Parameters
         ----------
         X: DataFrame, shape (n_samples, n_features)
             New data with n_samples as its number of samples.
-        
+
         Returns
         -------
         X_new: DataFrame, shape (n_samples_new, n_features)
             X data with substituted values to their respective labels being
-            string type. 
-        
+            string type.
+
         """
         try:
             getattr(self, 'bins_')
@@ -138,40 +137,40 @@ class Discretizer(BaseEstimator, TransformerMixin):
         X_new = pd.DataFrame()
         for col in X.columns.values:
             if is_numeric(X[col]):
-                try:
-                    X_new[col] = self.finger(
-                        X[col], 
-                        cut_points=self.bins_[col][1:-1]
-                    ).astype(str)
-                except UniqueValuesError as e:
-                    e.args += (f'The problem occured for the column: {col}.',)
-                    print(e)
-                    X_new[col] = X[col].astype(str)
+                cut_points = self.bins_[col][1:-1]
+
+                if not cut_points:
+                    cut_points = self.bins_[col]
+
+                X_new[col] = self.finger(
+                    X[col],
+                    cut_points=cut_points
+                ).astype(str)
             else:
                 X_new[col] = X[col].astype(str)
-        
+
         return X_new
 
     def sapling(self, X, y, **params):
-        """Creates finitely many intervals for a continuous vector using 
+        """Creates finitely many intervals for a continuous vector using
         DecisionTreeClassifier optimizing splits with respect to Gini impurity
         criterium.
 
         Parameters
         ----------
         X: array-like, shape (n_samples, )
-            Vector passed as an one-dimensional array-like object where 
+            Vector passed as an one-dimensional array-like object where
             n_samples in the number of samples.
 
         y: array-like, shape (n_samples, )
-            Vector of corresponding to X values passed as an one-dimensional 
+            Vector of corresponding to X values passed as an one-dimensional
             array-like object where n_samples is the number of samples.
 
         Returns
         -------
         bins: array, shape (n_bins, )
-            Vector of successive cut-off points being upper limits of the 
-            corresponding intervals as well as containing a minimum value. 
+            Vector of successive cut-off points being upper limits of the
+            corresponding intervals as well as containing a minimum value.
 
         """
         y = np.asarray(y)
@@ -181,20 +180,22 @@ class Discretizer(BaseEstimator, TransformerMixin):
 
         if len(np.unique(X)) < self.min_bins:
             raise UniqueValuesError(
-                'Not enough unique values in the array. ' 
+                'Not enough unique values in the array. '
                 'Minimum {} unique values required.'.format(self.min_bins)
             )
 
-        clf = DecisionTreeClassifier(max_depth=self.max_tree_depth,
-                                     min_samples_leaf=self.min_samples_leaf,
-                                     max_leaf_nodes=self.max_bins)
+        clf = DecisionTreeClassifier(
+            max_depth=self.max_tree_depth,
+            min_samples_leaf=self.min_samples_leaf,
+            max_leaf_nodes=self.max_bins
+        )
         X = X.reshape(-1, 1)
-        
+
         clf.fit(X, y)
-        
+
         min_val = min(X)
         max_val = max(X)
-        
+
         bins = clf.tree_.threshold[clf.tree_.feature != -2]
         np.append(bins, [min_val, max_val])
         bins = np.unique(bins)
@@ -202,7 +203,7 @@ class Discretizer(BaseEstimator, TransformerMixin):
         cols_out_idx = []
         for idx, val in enumerate(bins):
             if not (min_val <= val <= max_val):
-                cols_out_idx.append(idx) 
+                cols_out_idx.append(idx)
 
         bins = np.delete(bins, cols_out_idx)
 
@@ -215,18 +216,18 @@ class Discretizer(BaseEstimator, TransformerMixin):
         Parameters
         ----------
         X: array-like, shape (n_samples, )
-            Vector passed as an one-dimensional array-like object where 
+            Vector passed as an one-dimensional array-like object where
             n_samples is the number of samples.
 
         y: array-like, shape (n_samples, )
-            Vector of corresponding to X values passed as an one-dimensional 
+            Vector of corresponding to X values passed as an one-dimensional
             array-like object where n_samples is the number of samples.
 
         Returns
         -------
         bins: array, shape (n_bins, )
-            Vector of successive cut-off points being upper limits of the 
-            corresponding intervals as well as containing a minimum value. 
+            Vector of successive cut-off points being upper limits of the
+            corresponding intervals as well as containing a minimum value.
 
         """
         y = np.asarray(y)
@@ -235,53 +236,58 @@ class Discretizer(BaseEstimator, TransformerMixin):
         X = X[~np.isnan(X)]
 
         if len(np.unique(X)) < self.min_bins:
-            raise RuntimeError('not enough unique values in the array')
-        
+            raise UniqueValuesError(
+                'Not enough unique values in the array. '
+                'Minimum {} unique values required.'.format(self.min_bins)
+            )
+
         r = 0
         n = self.max_bins + 1
         while np.abs(r) < 1:
             bins = algos.quantile(np.unique(X), np.linspace(0, 1, n))
-            df = pd.DataFrame({'X': X, 
-                               'y': y, 
-                               'Bucket': pd.cut(X, bins=bins, 
-                                                include_lowest=True)
-                              })
+            df = pd.DataFrame({
+                'X': X,
+                'y': y,
+                'Bucket': pd.cut(X, bins=bins, include_lowest=True)
+            })
             df_gr = df.groupby(by='Bucket', as_index=True)
             if not (df_gr.agg('count').X == 0).any():
-                r, p = stats.spearmanr(getattr(df_gr, self.formula)().X,
-                                       getattr(df_gr, self.formula)().y) 
-                
+                r, p = stats.spearmanr(
+                    getattr(df_gr, self.formula)().X,
+                    getattr(df_gr, self.formula)().y
+                )
+
             if n == self.min_bins + 1:
                 break
 
             n -= 1
 
         return bins
-            
+
     @staticmethod
-    def finger(X, y=None, cut_points=None, 
-               n_quantiles=4, labels=None, 
+    def finger(X, y=None, cut_points=None,
+               n_quantiles=4, labels=None,
                min_val=None, max_val=None, **params):
         """Manually bins continuous variable into the declared intervals.
 
-        If the cut-off points are not declared the split is made using 
+        If the cut-off points are not declared the split is made using
         quantiles.
 
         Parameters
         ----------
         X: array-like, shape (n_samples, )
-            Vector passed as an one-dimensional array-like object where 
+            Vector passed as an one-dimensional array-like object where
             n_samples in the number of samples.
 
         y: Ignore
 
         cut_points: array-like
-            Increasing monotonic sequence generating right-closed intervals. 
-            Values not allocated to any of the categories will be assigned to 
+            Increasing monotonic sequence generating right-closed intervals.
+            Values not allocated to any of the categories will be assigned to
             the empty set. For example given: cut_points=[1, 5, 9] will
-            generate intervals: [X.min(), 1], (1, 5], (5, 9], (9, X.max()]. 
+            generate intervals: [X.min(), 1], (1, 5], (5, 9], (9, X.max()].
             If you want to specify lower and upper limitations, set parameters:
-            "min_val", "max_val" to a specific value. 
+            "min_val", "max_val" to a specific value.
 
         n_quantiles: int (default: 4)
             When cut_points are not declared it sets the number of quantiles
@@ -294,7 +300,7 @@ class Discretizer(BaseEstimator, TransformerMixin):
             number of created buckets.
             auto:
                 Assigns default values to group names by numbering them.
-            
+
         min_val: float
             Determines lower limit value. If not specified takes -np.inf.
 
@@ -304,37 +310,39 @@ class Discretizer(BaseEstimator, TransformerMixin):
         Returns
         -------
         X_new: array, shape (n_samples, )
-            Input data with its original values ​​being substituted with their 
+            Input data with its original values ​​being substituted with their
             respective labels.
 
         """
         X = np.asarray(X)
         x = X[~np.isnan(X)]
-        
-        if len(np.unique(x)) <= 1:
-            raise UniqueValuesError(
-                'The same value for every element of the array.')
 
         if min_val is None:
             min_val = -np.inf
 
-        if max_val is None:    
+        if max_val is None:
             max_val = np.inf
 
         # Default break_points in case of no declaration of cut_points
         if cut_points is None:
-            x = x[(x>=min_val) & (x<=max_val)]
-            break_points = algos.quantile(np.unique(x), 
-                                          np.linspace(0, 1, n_quantiles+1))
-        else:    
-            break_points = np.insert(cut_points, 
-                                     [0, len(cut_points)], 
-                                     [min_val, max_val])
+            x = x[(x >= min_val) & (x <= max_val)]
+            break_points = algos.quantile(
+                np.unique(x),
+                np.linspace(0, 1, n_quantiles + 1)
+            )
+        else:
+            break_points = np.insert(
+                cut_points.astype(float),
+                [0, len(cut_points)],
+                [min_val, max_val]
+            )
+        break_points = np.unique(break_points)
 
         if labels == 'auto':
-            labels = range(len(break_points)-1)
+            labels = range(len(break_points) - 1)
 
-        X_new = pd.cut(X, bins=break_points, 
-                       labels=labels, include_lowest=True)
+        X_new = pd.cut(
+            X, bins=break_points, labels=labels, include_lowest=True
+        )
 
         return X_new
