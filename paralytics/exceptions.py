e36f74408c8d9f84e8497b2090f1fce989319add
mrtovsky <emerte.quant@gmail.com>
2019-03-01 01:39:19 +0100

    @add: NothingSelectedWarning for notifying the user that nothing has been selected into an object that we would expect to be non-empty.

--- paralytics/exceptions.py
+++ paralytics/exceptions.py
@@ -1,12 +1,25 @@
 """
-This module includes all custom warnings and error classes used across 
+This module includes all custom warnings and error classes used across
 paralytics.
 """
-
-__all__ = ['UniqueValuesError']
+__all__ = [
+    'UniqueValuesError',
+    'NothingSelectedWarning'
+]
 
 
 class UniqueValuesError(ValueError):
     """Exception class to raise if number of unique values is not matching
     the required threshold.
+    """
+
+
+class NothingSelectedWarning(UserWarning):
+    """Warning used to notify user that nothing has been selected.
+
+    This warning notifies the user that after executing the script nothing
+    has been selected where it is expected not to receive an empty object.
+
+    For example we can thus emphasize that the user forgot to implement the
+    appropriate step in the pipeline.
     """
