f2be189fd18e7eeafa41616c74af0d3da6c6a841
mrtovsky <emerte.quant@gmail.com>
2019-03-16 22:41:33 +0100

    @fix: typo.

--- paralytics/utils/importing.py
+++ paralytics/utils/importing.py
@@ -4,14 +4,13 @@ from collections import OrderedDict
 
 
 def get_csv(in_filename, cols_to_keep=None, dtype=float):
-    """Get csv as list of OrderedDicts."""
-    
+    """Get csv as a list of OrderedDicts."""
     with open(in_filename, encoding='UTF-8') as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         if cols_to_keep is None:
             cols_to_keep = reader.fieldnames
         data = [
-            OrderedDict((key, dtype(row[key])) for key in cols_to_keep) 
+            OrderedDict((key, dtype(row[key])) for key in cols_to_keep)
             for row in reader
         ]
     return data
