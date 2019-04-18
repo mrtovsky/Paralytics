65d2ae1683c02d6304a44cf05e6467395563720e
mrtovsky <emerte.quant@gmail.com>
2019-04-18 18:00:03 +0200

    @modify: Single-sourcing the package version.

--- setup.py
+++ setup.py
@@ -1,16 +1,41 @@
-from setuptools import setup, find_packages
+import codecs
+import pathlib
+import re
+
+from setuptools import find_packages, setup
 
 
-with open('requirements.txt') as f:
+parent_path = pathlib.Path(__file__).parent
+
+
+def read(*files):
+    file_path = str(parent_path.joinpath(*files).resolve())
+    with codecs.open(file_path) as f:
+        return f.read()
+
+
+def find_version(*files):
+    version_file = read(*files)
+    version_pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.M)
+    version_match = version_pattern.search(version_file)
+    if version_match:
+        return version_match.group(1)
+    else:
+        raise RuntimeError('Unable to find version string.')
+
+
+with codecs.open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
+long_description = read('README.rst')
 
 setup(
     name='paralytics',
-    version='0.2.2',
+    version=find_version('paralytics', '__init__.py'),
     author='Mateusz Zakrzewski',
     author_email="paralytics@gmail.com",
     description='Python Analytical Scripts.',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     url='https://mrtovsky.github.io/Paralytics/',
     install_requires=requirements,
     license='MIT',
