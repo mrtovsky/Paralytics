68e4412d0d18a49093aabe9ec11a62d028df05f4
mrtovsky <emerte.quant@gmail.com>
2019-07-09 00:32:28 +0200

    @modify: Adaptaion to new names.

--- paralytics/__init__.py
+++ paralytics/__init__.py
@@ -1,11 +1,10 @@
 from . import preprocessing, utils, xai
-from .collinearity_reduction import *
 from .decorators import *
 from .discretization import *
 from .encoding import *
 from .exceptions import *
+from .feature_selection import *
 from .feature_union import *
-from .mathy import *
 
 
 __version__ = '0.3'
